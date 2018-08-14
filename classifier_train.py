9b5e61b5217fc25418486f513990d6ac4faf196c
zichun <zichun92@gmail.com>
2018-08-14 17:59:09 +0800

    add testing functiion for training

--- classifier_train.py
+++ classifier_train.py
@@ -9,6 +9,7 @@ from face_recognition.face_recognition_cli import image_files_in_folder
 import face_recognition as fr
 from sklearn import neighbors
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 import os
 import os.path
 import pickle
@@ -17,18 +18,30 @@ import time
 
 """
  Training images directory structure:
-    <train_dir>/
+    <train>/
     ├── <person1>/
-    │   ├── <somename1>.jpeg
-    │   ├── <somename2>.jpeg
+    │   ├── <somename1>.jpg
+    │   ├── <somename2>.jpg
     │   ├── ...
     ├── <person2>/
-    │   ├── <somename1>.jpeg
-    │   └── <somename2>.jpeg
+    │   ├── <somename1>.jpg
+    │   └── <somename2>.jpg
+    └── ...
+
+Testing images directory structure:
+    <test>/
+    ├── <person1>/
+    │   ├── <somename1>.jpg
+    │   ├── <somename2>.jpg
+    │   ├── ...
+    ├── <person2>/
+    │   ├── <somename1>.jpg
+    │   └── <somename2>.jpg
     └── ...
 """
 
 TRAIN_DATA_PATH = "faces/train"
+TEST_DATA_PATH = "faces/test"
 KNN_SAVE_PATH = "classifier/trained_knn_model.clf"
 LSVM_SAVE_PATH = "classifier/trained_lsvm_model.clf"
 
@@ -38,12 +51,17 @@ class ClassifierTrain:
         self.method = method
         self.X_train = []
         self.y_train = []
+        self.total_persons_train = 0
+        self.total_images_train = 0
         self.X_test = []
         self.y_test = []
+        self.total_persons_test = 0
+        self.total_images_test = 0
     
     def start(self):
         print("[INFO] It's going to train a face classifer.")
-        self.prepare_data_for_train()
+        self.X_train, self.y_train = self.prepare_data(TRAIN_DATA_PATH)
+        self.X_test, self.y_test = self.prepare_data(TEST_DATA_PATH)
 
         if self.method == 'LSVM':
             self.lsvm_train()
@@ -53,31 +71,44 @@ class ClassifierTrain:
             self.lsvm_train()
             self.knn_train(train_n_neighbors=7)
     
-    def prepare_data_for_train(self):
+    def prepare_data(self, path):
         time_start = time.time()
+
+        if "train" in path:
+            mode = "training"
+        elif "test" in path:
+            mode = "testing"
+        
+        print("[INFO] Start to prepare data for {}...".format(mode))
+
+        X = []
+        y = []
+
         # Loop through each person in the training set
-        for class_dir in os.listdir(TRAIN_DATA_PATH):
-            if not os.path.isdir(os.path.join(TRAIN_DATA_PATH, class_dir)):
+        for class_dir in os.listdir(path):
+            if not os.path.isdir(os.path.join(path, class_dir)):
                 continue
 
             # Loop through each training image for the current person
-            for image_path in image_files_in_folder(os.path.join(TRAIN_DATA_PATH, class_dir)):
+            for image_path in image_files_in_folder(os.path.join(path, class_dir)):
                 image = fr.load_image_file(image_path)
                 faces_boxes = fr.face_locations(
                     image, number_of_times_to_upsample=1)
 
                 if len(faces_boxes) != 1:
                     # If there are no people (or too many people) in a training image, skip the image.
-                    print("[WARNING] Image {} not suitable for training: {}".format(
-                        image_path, "Didn't find a face" if len(faces_boxes) < 1 else "Found more than one face"))
+                    print("[WARNING] Image {} not suitable for {}: {}".format(
+                        image_path, mode, "Didn't find a face" if len(faces_boxes) < 1 else "Found more than one face"))
                 else:
                     # Add face encoding for current image to the training set
-                    self.X_train.append(fr.face_encodings(
+                    X.append(fr.face_encodings(
                         image, known_face_locations=faces_boxes)[0])
-                    self.y_train.append(class_dir)
+                    y.append(class_dir)
         time_end = time.time()
         time_spent = time_end - time_start
-        print("[INFO] Data has been prepared well for training. Time spent: {:.3f}s".format(time_spent))
+        print("[INFO] Data has been prepared well for {}. Time spent: {:.3f}s".format(mode, time_spent))
+        
+        return X, y
 
     def knn_train(self, train_n_neighbors):
         print("[INFO] Start to train KNN classifier...")
@@ -93,6 +124,7 @@ class ClassifierTrain:
         trained_knn_clf = neighbors.KNeighborsClassifier(
             n_neighbors=train_n_neighbors, algorithm='ball_tree', weights='distance')
         trained_knn_clf.fit(self.X_train, self.y_train)
+        acc_knn = accuracy_score(self.y_test, trained_knn_clf.predict(self.X_test))
 
         # Save the trained KNN classifier
         if KNN_SAVE_PATH is not None:
@@ -100,17 +132,20 @@ class ClassifierTrain:
                 pickle.dump(trained_knn_clf, f)
                 time_end = time.time()
                 time_spent = time_end - time_start
-                print("[INFO] Training completed! KNN Classifier saved! Time spent: {:.3f}s".format(time_spent))
+                print("[INFO] Training completed! KNN Classifier saved! Accuracy:{}, Time spent: {:.3f}s".format(acc_knn, time_spent))
 
     def lsvm_train(self):
         print("[INFO] Start to train Linear SVM classifier...")
         time_start = time.time()
+
         trained_lsvm_clf = LinearSVC()
         trained_lsvm_clf.fit(self.X_train, self.y_train)
+        acc_svm = accuracy_score(self.y_test, trained_lsvm_clf.predict(self.X_test))
+
         # Save the trained SVM classifier
         if LSVM_SAVE_PATH is not None:
             with open(LSVM_SAVE_PATH, 'wb') as f:
                 pickle.dump(trained_lsvm_clf, f)
                 time_end = time.time()
                 time_spent = time_end - time_start
-                print("[INFO] Training completed! SVM Classifier saved! Time spent: {:.3f}s".format(time_spent))
+                print("[INFO] Training completed! SVM Classifier saved! Accuracy:{}, Time spent: {:.3f}s".format(acc_svm, time_spent))
